fc2596342c6ba930ad3b173d9957d0e86c2b05d3
Nahum Fabian <nahum.fabian@affiliate.oliverwyman.com>
2019-06-24 00:00:33 -0600

    make use of Redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers'
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, storeEnhancers(applyMiddleware(thunk)))
 
 export default store
