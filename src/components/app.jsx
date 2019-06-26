0b95d219646e69e33349170fe8eb711b3aaec6b8
Nahum Fabian <nahum.fabian@affiliate.oliverwyman.com>
2019-06-26 06:52:16 -0600

    add 404 route

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import '../index.css';
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter, Link } from 'react-router-dom'
 import Login from './auth/login'
 import PrivateRoute from './auth/private-route'
 import Home from './home'
-//import { ConnectedRouter } from 'react-router-redux';
+import MyVideos from './user/my-videos'
+
+const NotFound = () => (<div style={{margin:50}}>
+    <h4>THIS PAGE DOES NOT EXIST. 404 - NOT FOUND</h4>
+    <div>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/user/videos">My saved videos</Link></li>
+        </ul>
+    </div>
+</div>)
 
 const App = props => {
-    //const { history } = props
     return (
         <BrowserRouter>
             <div>
                 <Switch>
                     <PrivateRoute exact={true} path='/' component={Home}/>
+                    <PrivateRoute path='/user/videos' component={MyVideos} />
                     <Route path='/login' component={Login}/>
+                    <Route path='*' component={NotFound}/>
                 </Switch>
             </div>
         </BrowserRouter>
